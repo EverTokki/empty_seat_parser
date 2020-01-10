87ec6fe71a14bc74f0191aeefce8f97709f46cf2
EverTokki <chanbin.lee123@gmail.com>
2020-01-09 18:56:37 -0800

    Tweaks to fit my needs

--- test.py
+++ test.py
@@ -6,12 +6,9 @@ def get_students(url):
     soup = BeautifulSoup(html)
 
     table = soup.find_all('table')[3]
-    rows = table.find_all('tr')[1:] #[1:] skips header
-
     row = table.find_all('tr')[2]
-    for row in rows:
-        cols = row.find_all('td')[1]
-        print (cols.get_text())
+    col = row.find_all('td')[1]
+    print (col.get_text())
 
 def main():
     url = 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&submit=Register%20Selected&wldel=PSYC%2C365%2C001'
